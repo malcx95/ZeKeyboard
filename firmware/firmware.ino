c91555df5ef2624ce68388c295df5905156f261a
Malcolm Wigren <trekommafem2@gmail.com>
2016-11-22 12:18:23 +0100

    It works now... again

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,8 +1,29 @@
+#include <Tlc5940.h>
+#include <tlc_shifts.h>
+#include <tlc_animations.h>
+#include <tlc_config.h>
+#include <tlc_fades.h>
+#include <tlc_servos.h>
+#include <tlc_progmem_utils.h>
+
 #include "keyboard.h"
 
 Ze::Board keyboard;
+Tlc5940 tlc;
 
 void setup() {
+
+    delay(50);
+    
+    tlc.init();
+
+    delay(200);
+    
+    tlc.setAll(3000);
+    
+    delay(200);
+
+    tlc.update();
 
     keyboard.init();
 
