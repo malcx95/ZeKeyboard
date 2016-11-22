604a1dbdd98981e6200c11a65b60800582b473cc
Malcolm Wigren <trekommafem2@gmail.com>
2016-11-22 12:02:43 +0100

    Reverted to code where escape works

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,9 +1,25 @@
+#include <Tlc5940.h>
+#include <tlc_shifts.h>
+#include <tlc_animations.h>
+#include <tlc_config.h>
+#include <tlc_fades.h>
+#include <tlc_servos.h>
+#include <tlc_progmem_utils.h>
+
+
 #include "keyboard.h"
 
 Ze::Board keyboard;
+Tlc5940 tlc;
 
 void setup() {
 
+    tlc.init();
+    delay(200);
+    tlc.setAll(3000);
+    delay(200);
+    tlc.update();
+    delay(200);
     keyboard.init();
 
     Serial.begin(9600);
