39a1c735f651bc0e613ca74a1d704aa60cb591a5
Malcolm Vigren <trekommafem2@gmail.com>
2023-10-04 21:59:37 +0200

    Add rust boilerplate

---
+++ rust-test/setup.sh
@@ -0,0 +1,8 @@
+rustup target add thumbv7em-none-eabi
+rustup target add thumbv7em-none-eabihf
+
+cargo install cargo-generate
+cargo install cargo-binutils
+rustup component add llvm-tools-preview
+
+sudo apt install libusb-dev
