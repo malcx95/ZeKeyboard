55fb3e3086934a5d54350c26e8e5022b3e195946
Malcolm Vigren <trekommafem2@gmail.com>
2023-10-14 14:04:04 +0200

    WIP: Continue rust experiment

--- newfirmware/newfirmware.ino
+++ newfirmware/newfirmware.ino
@@ -1,28 +1,12 @@
-#include <KeyboardController.h>
-#include <MouseController.h>
-#include <Usb.h>
-#include <address.h>
-#include <adk.h>
-#include <confdescparser.h>
-#include <hid.h>
-#include <hidboot.h>
-#include <hidusagestr.h>
-#include <parsetools.h>
-#include <usb_ch9.h>
-
-#include <Keyboard.h>
-#include <KeyboardLayout.h>
-
 #include <Keyboard.h>
 
 #include "src/firmware.hpp"
-#include "test.h"
+
 
 Firmware firmware;
 
 void setup() {
     firmware.setup();
-    test();
 }
 
 void loop() {
--- rust-test/firmware/src/main.rs
+++ rust-test/firmware/src/main.rs
@@ -1,100 +1,215 @@
-//! The starter code slowly blinks the LED, sets up
-//! USB logging, and creates a UART driver using pins
-//! 14 and 15. The UART baud rate is [`UART_BAUD`].
+//! Demonstrates a USB keypress using RTIC.
 //!
-//! Despite targeting the Teensy 4.0, this starter code
-//! also works on the Teensy 4.1.
+//! Flash your board with this example. Your device will occasionally
+//! send some kind of keypress to your host.
 
 #![no_std]
 #![no_main]
 
-use bsp::board;
-use teensy4_bsp as bsp;
-use teensy4_panic as _;
+use teensy4_bsp::ral;
 
-use bsp::hal::timer::Blocking;
+#[rtic::app(device = teensy4_bsp::ral, peripherals = false)]
+mod app {
 
-use core::fmt::Write as _;
+    use teensy4_bsp as bsp;
+    use bsp::hal;
+    use bsp::board;
 
-/// CHANGE ME to vary the baud rate.
-const UART_BAUD: u32 = 115200;
-/// Milliseconds to delay before toggling the LED
-/// and writing text outputs.
-const DELAY_MS: u32 = 1000;
+    use hal::usbd::{BusAdapter, EndpointMemory, EndpointState, Speed};
 
-#[bsp::rt::entry]
-fn main() -> ! {
-    // These are peripheral instances. Let the board configure these for us.
-    // This function can only be called once!
-    let instances = board::instances();
+    use usb_device::{
+        bus::UsbBusAllocator,
+        device::{UsbDevice, UsbDeviceBuilder, UsbDeviceState, UsbVidPid},
+    };
+    use usbd_hid::{
+        descriptor::{KeyboardReport, SerializedDescriptor as _},
+        hid_class::HIDClass,
+    };
 
-    // Driver resources that are configured by the board. For more information,
-    // see the `board` documentation.
-    let board::Resources {
-        // `pins` has objects that represent the physical pins. The object
-        // for pin 13 is `p13`.
-        pins,
-        // This is a hardware timer. We'll use it for blocking delays.
-        mut gpt1,
-        // These are low-level USB resources. We'll pass these to a function
-        // that sets up USB logging.
-        usb,
-        // This is the GPIO2 port. We need this to configure the LED as a
-        // GPIO output.
-        mut gpio2,
-        // This resource is for the UART we're creating.
-        lpuart2,
-        ..
-    } = board::t40(instances);
+    /// Change me if you want to play with a full-speed USB device.
+    const SPEED: Speed = Speed::High;
+    /// Matches whatever is in imxrt-log.
+    const VID_PID: UsbVidPid = UsbVidPid(0x5824, 0x27dd);
+    const PRODUCT: &str = "imxrt-hal-example";
+    /// How frequently should we poll the logger?
+    const LPUART_POLL_INTERVAL_MS: u32 = board::UART_FREQUENCY / 1_000 * 100;
+    /// Change me to change how log messages are serialized.
+    ///
+    /// If changing to `Defmt`, you'll need to update the logging macros in
+    /// this example. You'll also need to make sure the USB device you're debugging
+    /// uses `defmt`.
+    const FRONTEND: bsp::logging::log = bsp::logging::log::Log;
+    /// The USB GPT timer we use to (infrequently) send mouse updates.
+    const GPT_INSTANCE: imxrt_usbd::gpt::Instance = imxrt_usbd::gpt::Instance::Gpt0;
+    /// How frequently should we push mouse updates to the host?
+    const MOUSE_UPDATE_INTERVAL_MS: u32 = 200;
 
-    // When this returns, you can use the `log` crate to write text
-    // over USB. Use either `screen` (macOS, Linux) or PuTTY (Windows)
-    // to visualize the messages from this example.
-    bsp::LoggingFrontend::default_log().register_usb(usb);
+    /// This allocation is shared across all USB endpoints. It needs to be large
+    /// enough to hold the maximum packet size for *all* endpoints. If you start
+    /// noticing panics, check to make sure that this is large enough for all endpoints.
+    static EP_MEMORY: EndpointMemory<1024> = EndpointMemory::new();
+    /// This manages the endpoints. It's large enough to hold the maximum number
+    /// of endpoints; we're not using all the endpoints in this example.
+    static EP_STATE: EndpointState = EndpointState::max_endpoints();
 
-    // This configures the LED as a GPIO output.
-    let led = board::led(&mut gpio2, pins.p13);
+    use core::{iter::Cycle, slice::Iter};
+    type MessageIter = Cycle<Iter<'static, u8>>;
+    const MESSAGE: &[u8] = b"Ia! Ia! Cthulhu fhtagn!  ";
 
-    // Configures the GPT1 timer to run at GPT1_FREQUENCY. See the
-    // constants below for more information.
-    gpt1.disable();
-    gpt1.set_divider(GPT1_DIVIDER);
-    gpt1.set_clock_source(GPT1_CLOCK_SOURCE);
+    type Bus = BusAdapter;
 
-    // Convenience for blocking delays.
-    let mut delay = Blocking::<_, GPT1_FREQUENCY>::from_gpt(gpt1);
+    #[local]
+    struct Local {
+        class: HIDClass<'static, Bus>,
+        device: UsbDevice<'static, Bus>,
+        led: board::Led,
+        poller: board::logging::Poller,
+        timer: hal::pit::Pit<0>,
+        message: MessageIter,
+    }
 
-    // Create the UART driver using pins 14 and 15.
-    // Cast it to a embedded_hal trait object so we can
-    // use it with the write! macro.
-    let mut lpuart2: board::Lpuart2 = board::lpuart(lpuart2, pins.p14, pins.p15, UART_BAUD);
-    let lpuart2: &mut dyn embedded_hal::serial::Write<u8, Error = _> = &mut lpuart2;
+    #[shared]
+    struct Shared {}
 
-    let mut counter: u32 = 0;
-    loop {
-        led.toggle();
-        log::info!("Hello from the USB logger! The count is {counter}");
-        write!(
-            lpuart2,
-            "Hello from the UART driver! The count is {counter}\r\n"
+    #[init(local = [bus: Option<UsbBusAllocator<Bus>> = None])]
+    fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
+        let (
+            board::Common {
+                pit: (mut timer, _, _, _),
+                usb1,
+                usbnc1,
+                usbphy1,
+                mut dma,
+                ..
+            },
+            board::Specifics { led, console, .. },
+        ) = board::new();
+        timer.set_load_timer_value(LPUART_POLL_INTERVAL_MS);
+        timer.set_interrupt_enable(true);
+        timer.enable();
+
+        let dma_a = dma[board::BOARD_DMA_A_INDEX].take().unwrap();
+        let poller = board::logging::lpuart(FRONTEND, console, dma_a);
+
+        let usbd = hal::usbd::Instances {
+            usb: usb1,
+            usbnc: usbnc1,
+            usbphy: usbphy1,
+        };
+
+        let bus = BusAdapter::with_speed(usbd, &EP_MEMORY, &EP_STATE, SPEED);
+        bus.set_interrupts(true);
+        bus.gpt_mut(GPT_INSTANCE, |gpt| {
+            gpt.stop();
+            gpt.clear_elapsed();
+            gpt.set_interrupt_enabled(true);
+            gpt.set_mode(imxrt_usbd::gpt::Mode::Repeat);
+            gpt.set_load(MOUSE_UPDATE_INTERVAL_MS * 1000);
+            gpt.reset();
+            gpt.run();
+        });
+
+        let bus = ctx.local.bus.insert(UsbBusAllocator::new(bus));
+        // Note that "4" correlates to a 1ms polling interval. Since this is a high speed
+        // device, bInterval is computed differently.
+        let class = HIDClass::new(bus, KeyboardReport::desc(), 4);
+        let device = UsbDeviceBuilder::new(bus, VID_PID)
+            .product(PRODUCT)
+            .device_class(usbd_serial::USB_CLASS_CDC)
+            .max_packet_size_0(64)
+            .build();
+
+        (
+            Shared {},
+            Local {
+                class,
+                device,
+                led,
+                poller,
+                timer,
+                message: MESSAGE.iter().cycle(),
+            },
+            init::Monotonics(),
         )
-        .ok();
+    }
 
-        delay.block_ms(DELAY_MS);
-        counter = counter.wrapping_add(1);
+    #[task(binds = BOARD_PIT, local = [poller, timer], priority = 1)]
+    fn pit_interrupt(ctx: pit_interrupt::Context) {
+        while ctx.local.timer.is_elapsed() {
+            ctx.local.timer.clear_elapsed();
+        }
+
+        ctx.local.poller.poll();
+    }
+
+    #[task(binds = BOARD_USB1, local = [device, class, led, message, configured: bool = false], priority = 2)]
+    fn usb1(ctx: usb1::Context) {
+        let usb1::LocalResources {
+            class,
+            device,
+            led,
+            configured,
+            message,
+        } = ctx.local;
+
+        device.poll(&mut [class]);
+
+        if device.state() == UsbDeviceState::Configured {
+            if !*configured {
+                device.bus().configure();
+            }
+            *configured = true;
+        } else {
+            *configured = false;
+        }
+
+        if *configured {
+            let elapsed = device.bus().gpt_mut(GPT_INSTANCE, |gpt| {
+                let elapsed = gpt.is_elapsed();
+                while gpt.is_elapsed() {
+                    gpt.clear_elapsed();
+                }
+                elapsed
+            });
+
+            if elapsed {
+                led.toggle();
+                let code = *message.next().unwrap();
+                if let Some(report) = translate_char(code) {
+                    class.push_input(&report).ok();
+                }
+            }
+        }
+    }
+
+    fn translate_char(ch: u8) -> Option<KeyboardReport> {
+        fn simple_kr(modifier: u8, keycodes: [u8; 6]) -> Option<KeyboardReport> {
+            Some(KeyboardReport {
+                modifier,
+                reserved: 0,
+                leds: 0,
+                keycodes,
+            })
+        }
+
+        match ch {
+            b'a'..=b'z' => {
+                let code = ch - b'a' + 4;
+                simple_kr(0, [code, 0, 0, 0, 0, 0])
+            }
+            b'A'..=b'Z' => {
+                let code = ch - b'A' + 4;
+                simple_kr(2, [code, 0, 0, 0, 0, 0])
+            }
+            b'!'..=b')' => {
+                let code = ch - b'!' + 0x1e;
+                simple_kr(2, [code, 0, 0, 0, 0, 0])
+            }
+            b' ' => simple_kr(0, [0x2c, 0, 0, 0, 0, 0]),
+            _ => {
+                log::error!("Unsupported character '{}'", ch);
+                None
+            }
+        }
     }
 }
-
-// We're responsible for configuring our timers.
-// This example uses PERCLK_CLK as the GPT1 clock source,
-// and it configures a 1 KHz GPT1 frequency by computing a
-// GPT1 divider.
-use bsp::hal::gpt::ClockSource;
-
-/// The intended GPT1 frequency (Hz).
-const GPT1_FREQUENCY: u32 = 1_000;
-/// Given this clock source...
-const GPT1_CLOCK_SOURCE: ClockSource = ClockSource::HighFrequencyReferenceClock;
-/// ... the root clock is PERCLK_CLK. To configure a GPT1 frequency,
-/// we need a divider of...
-const GPT1_DIVIDER: u32 = board::PERCLK_FREQUENCY / GPT1_FREQUENCY;
